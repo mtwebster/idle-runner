6007adfae3b3ebe23dcdaabb3bd950daf9ec8f63
Michael Webster <miketwebster@gmail.com>
2019-03-02 13:27:54 -0500

    Use systemd service instead of autostart spec

--- src/idle-runner.py
+++ src/idle-runner.py
@@ -6,6 +6,7 @@ import setproctitle
 from gi.repository import GLib, Gio
 
 signal.signal(signal.SIGINT, signal.SIG_DFL)
+signal.signal(signal.SIGTERM, signal.SIG_DFL)
 
 # https://github.com/linuxmint/cinnamon-session/blob/master/cinnamon-session/org.gnome.SessionManager.Presence.xml#L13
 IDLE = 3 
@@ -15,21 +16,21 @@ class Main:
         self.settings = Gio.Settings(schema_id="org.webster.idlerunner")
 
         try:
-            self.proxy = Gio.DBusProxy.new_for_bus_sync(Gio.BusType.SESSION,
-                                                        Gio.DBusProxyFlags.DO_NOT_AUTO_START,
-                                                        None,
-                                                        "org.gnome.SessionManager",
-                                                        "/org/gnome/SessionManager/Presence",
-                                                        "org.gnome.SessionManager.Presence",
-                                                        None)
+            self.presence_proxy = Gio.DBusProxy.new_for_bus_sync(Gio.BusType.SESSION,
+                                                                 Gio.DBusProxyFlags.DO_NOT_AUTO_START,
+                                                                 None,
+                                                                 "org.gnome.SessionManager",
+                                                                 "/org/gnome/SessionManager/Presence",
+                                                                 "org.gnome.SessionManager.Presence",
+                                                                 None)
 
-            self.proxy.connect("g-signal",
-                               self.on_proxy_signal)
+            self.presence_proxy.connect("g-signal",
+                                        self.on_presence_proxy_signal)
         except GLib.Error as e:
-            print("idle-runner: Could not connect to session manager - %s" % e.message)
+            print("idle-runner: Could not connect to session manager presence- %s" % e.message)
             exit(1)
 
-    def on_proxy_signal(self, proxy, sender, signal, params, data=None):
+    def on_presence_proxy_signal(self, proxy, sender, signal, params, data=None):
         if signal == "StatusChanged":
             if params[0] == IDLE:
                 self.run_script()
